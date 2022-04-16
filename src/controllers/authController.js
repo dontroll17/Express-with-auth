2d31da5cf619f6b66ddc9ad1493a4723ddb72afe
dontoll17 <dontroll@yandex.ru>
2022-04-16 19:01:31 +0300

    feat: dasboard route and page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,3 +3,7 @@ const { resolve } = require('path');
 exports.signup = (req, res) => {
     res.sendFile(resolve(__dirname + '../../../views/signup.html'))
 }
+
+exports.dashboard = (req, res) => {
+    res.sendFile(resolve(__dirname + '../../../views/dashboard.html'))
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,8 @@ const authController = require('../controllers/authController.js');
 
 module.exports = (app, passport) => {
     app.get('/signup', authController.signup);
+    app.get('/dashboard', authController.dashboard);
+    
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard',
         failureRedirect: '/signup'
